eda4aeb31783d0211e057eaff93d850d287be2c2
peterberekvolgyi <berekvolgyipeter@gmail.com>
2025-01-31 12:23:41 +0100

    switch to 0.8.24 compiler version because of ccip

--- src/interfaces/IRebaseToken.sol
+++ src/interfaces/IRebaseToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.27;
+pragma solidity ^0.8.24;
 
 interface IRebaseToken {
     function mint(address _to, uint256 _amount, uint256 _userInterestRate) external;
--- test/RebaseToken.t.sol
+++ test/RebaseToken.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.27;
+pragma solidity ^0.8.24;
 
 import {console, Test} from "forge-std/Test.sol";
 import {IAccessControl} from "@openzeppelin/contracts/access/IAccessControl.sol";
